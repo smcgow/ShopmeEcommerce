a7efa85005a94d6fafb240b11010ae5c14d0bf4e
smcgow <stepheneoinmcgowan@gmail.com>
2022-01-09 21:51:55 +0000

    Creating role repository, role, role repository unit test.

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,33 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 40, nullable = false, unique = true)
+	private String name;
+	
+	@Column(length = 150, nullable = false)
+	private String description;
+	
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+}
