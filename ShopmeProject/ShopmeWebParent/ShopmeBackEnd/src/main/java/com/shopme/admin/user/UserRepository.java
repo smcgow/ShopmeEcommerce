f9ce251e3e75355a1a72e9a2c3312f6b46321e6c
smcgow <stepheneoinmcgowan@gmail.com>
2022-01-12 13:32:45 +0000

    Checking email uniqueness. Rest service called by AJAX using JQQuery to validate email. Present modal if the email is not unique.

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,9 +1,13 @@
 package com.shopme.admin.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shopme.common.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	public User getUserByEmail(@Param("email") String email);
 }
