051a7e55b9236a270386fea87294c5d5003aa8c4
smcgow <stepheneoinmcgowan@gmail.com>
2022-01-21 21:36:35 +0000

    Creating the new category form.

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,31 @@ public class CategoryService {
 	
 	public List<Category> searchByKeyword(String keyword){
 		return categoryRepository.findByNameLikeOrAliasLike("%" + keyword +  "%", "%" + keyword +  "%");
+	}
+	
+	public List<Category> listCategoriesinHierArchicalForm(){
+		List<Category> hierarchicalCategories = new ArrayList<>();
+		List<Category> categories = categoryRepository.findAll();
+		categories.forEach(category -> {
+			if(category.getParent() == null) {
+				hierarchicalCategories.add(category);
+				recurseChildren(category,0,hierarchicalCategories);
+			}
+		});
+		return hierarchicalCategories;
+	}
+	
+	private void recurseChildren(Category category, int level, List<Category> hierarchicalCategories) {
+		int newLevel = level+1;
+		category.getChildren().forEach(child -> {
+			String prefix = "";
+			for(int i = 0; i < newLevel; i ++) {
+				prefix += "--";
+			}
+			child.setName(prefix + " " + child.getName());
+			hierarchicalCategories.add(child);
+			recurseChildren(child, newLevel, hierarchicalCategories);
+		});
 	}
 
 }
