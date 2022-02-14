5bdf74fb668ca7b911d386dba158d5219a8192f5
smcgow <stepheneoinmcgowan@gmail.com>
2022-02-14 18:04:19 +0000

    Create entity and repository for brand

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -0,0 +1,49 @@
+package com.shopme.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 45, unique = true)
+	private String name;
+	
+	@Column(nullable = false, length = 128)
+	private String logo;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "brand_categories",
+			joinColumns = @JoinColumn(name = "brand_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id")
+			)
+	Set<Category> categories = new HashSet<>();
+	
+}
